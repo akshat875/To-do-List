cc6d66f92609a4237b1eec41d0b744ef08b14920
Akshat <akshatvishwakarma958@gmail.com>
2023-08-09 18:54:53 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,112 @@
+const inputEl = document.querySelector("#input");
+
+const outputEl = document.querySelector("#list-container");
+const form = document.querySelector("form");
+
+
+
+
+// //Add Task
+// localStorage.setItem("Firstnam","Emmanuel")
+// //Get item
+// const result=localStorage.getItem("firstname")
+// //remove
+// localStorage.removeItem("firstname")
+
+
+//Add Task and save into loclal storage
+const addTask = e => {
+  e.preventDefault();
+  //check if input is empty
+  if (inputEl.value === "") {
+    alert("Pleast enter a task");
+  }
+
+  //get the item
+  const task = inputEl.value;
+  if (task) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+      tasks = [];
+     
+    } else {
+      tasks = JSON.parse(localStorage.getItem("tasks"));
+    
+    }
+    tasks.unshift({
+      id: Date.now(),
+      title: task,
+    });
+    //Save to storage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    //empty input
+    inputEl.value = "";
+  }
+  getTasks();
+};
+form.addEventListener("submit", addTask);
+
+
+
+//get tasks
+const getTasks = () => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  //Display to DOM
+  let output;
+  const allTasks = tasks.map(task => {
+    return `
+    <li id="item">
+            <span>${task.title}</span>
+            <button onclick="EditTask('${task.id}')" id="Edit">edit</button>
+            <button onclick="removeTask('${task.id}')" id="delete">X</button>
+          </li>
+    `;
+  });
+  output = allTasks.join("");
+  outputEl.innerHTML = output;
+};
+getTasks();
+
+//Get items
+
+//remove
+
+//Event Listener
+
+//Delete task
+
+const removeTask = id => {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks", tasks));
+  }
+  tasks = tasks.filter(task => {
+    return task.id !== +id;
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  getTasks();
+};
+
+
+//Edit Task
+const EditTask = id=>{
+  let tasks;
+  if(localStorage.getItem("tasks") === null) {
+    tasks =[];
+  }
+  else{
+    tasks=JSON.parse(localStorage.getItem("tasks", tasks));
+
+}
+
+
+
+getTasks();
+}
